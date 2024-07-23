25460137f625aa803a4eb34c7d4ec4a3c94ea54f
leonardo <leopereirasilva86@gmail.com>
2024-07-23 13:23:28 -0300

    equipmentList grid created

--- frontend/src/pages/EquipmentList/EquipmentList.jsx
+++ frontend/src/pages/EquipmentList/EquipmentList.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styles from "./EquipmentList.module.css"
+import { useFetch } from '../../hooks/useFetch';
+import { FaPlus, FaFilter, FaHandshake, FaSearch } from "react-icons/fa";
+
+
+const url = "http://localhost:3000/equipment"
 
 const EquipmentList = () => {
+  const [searchQuery, setSearchQuery] = useState("Pesquisar");
+  const {data: equipments} = useFetch(url);
+  
   return (
     <div>
-        <h1>EquipmentList</h1>
+        <div className='topListBar'>
+          <div className='searchBar'>
+            <input type="search" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value) }/>
+          </div>
+          <div className='topListButtons'>
+            <FaPlus className={styles.newItemIcon}/>
+            <FaFilter className={styles.filterIcon}/>
+          </div>        
+        </div>
+        <div>
+          <ul className={styles.items}>
+            {equipments && equipments.map((equipment) => (
+              <li key={equipment.id}>
+                <div className={styles.equipmentBox}>
+                  <h2>{equipment.name}</h2>
+                  <p>{equipment.status}</p>
+                  <button>
+                    <FaHandshake />
+                  </button>
+                  <button>
+                    <FaSearch />
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>      
+        </div>       
     </div>
   )
 }
