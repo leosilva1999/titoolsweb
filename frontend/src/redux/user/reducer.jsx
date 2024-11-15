4e5a689e21b42f7173e9b5b8e2dc007b75a3cabe
leonardo <leopereirasilva86@gmail.com>
2024-11-15 14:17:20 -0300

    redux initial setup created

--- frontend/src/redux/user/reducer.jsx
+++ frontend/src/redux/user/reducer.jsx
@@ -1,3 +1,11 @@
-const userReducer = () => {};
+const initialState = {
+    currentUser: null,
+};
+
+const userReducer = (state = initialState, action) => {
+    if(action.type == "user/login"){
+        return null;
+    }
+};
 
 export default userReducer;
